2b3646f419f7b55a897049630c46ee1686eff473
jennyhuoh <huoh.li@gmail.com>
2021-07-29 09:40:16 +0800

    fix bug Todolist.js

--- 20210728-api/routes/json.js
+++ 20210728-api/routes/json.js
@@ -4,9 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.json([
-        {name: '讀書', complated: false, id: "i1"},
-        {name: '寫作業', complated: false, id: "i2"},
-        {name: '畫畫', complated: false, id: "i3"},
+        {name: '讀書', completed: false, id: "i1"},
+        {name: '寫作業', completed: false, id: "i2"},
+        {name: '畫畫', completed: false, id: "i3"},
     ])
 });
 
